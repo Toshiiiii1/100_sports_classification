bc93c152da3e78e2fcb58760868c7d832bf9ca47
Toshi <huynhduykhoi619@gmail.com>
2025-05-06 14:00:43 +0700

    chore

--- checkpoints/checkpoint_download.py
+++ checkpoints/checkpoint_download.py
@@ -2,6 +2,6 @@ import gdown
 from config import model_url
 
 if __name__ == "__main__":
-    # Download model from Google Drive
+    # download model from Google Drive
     gdown.download(url=model_url, output="checkpoints/100_sport_clf.keras", quiet=False, fuzzy=True)
     print("Model downloaded successfully.")
