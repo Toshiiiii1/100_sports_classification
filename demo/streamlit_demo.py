d6b5b71018007ad3412e1982f4330032f9156500
Toshi <huynhduykhoi619@gmail.com>
2025-05-06 13:59:57 +0700

    small demo with Streamlit UI

---
+++ demo/streamlit_demo.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import os
+import keras
+import config
+import io
+from PIL import Image
+import numpy as np
+
+if __name__ == "__main__":
+    # define model
+    clf_model = keras.saving.load_model(config.model_path)
+    img_uploaded = st.file_uploader("Choose an image", type=["jpg", "png"])
+    if img_uploaded is not None:
+        # load image as bytes
+        img_bytes = img_uploaded.read()
+        # convert bytes to PIL image
+        img = Image.open(io.BytesIO(img_bytes)).resize((config.img_width, config.img_heigth)) # 224x224
+        st.image(img)
+        img_array = keras.utils.img_to_array(img)
+        batch_img = np.array([img])
+        predictions = clf_model.predict(batch_img)
+        predict_class = np.argmax(predictions, axis=1)
+        st.write(config.int2label[predict_class[0]])
