c6ec23ee22eac9ea4894f73db1bf66a06827de5f
Toshi <huynhduykhoi619@gmail.com>
2025-05-08 15:47:18 +0700

    script for download model from GG Drive url

--- checkpoints/checkpoint_download.py
+++ checkpoints/checkpoint_download.py
@@ -1,7 +1,20 @@
 import gdown
-from config import model_url
+import argparse
+
+def parse_opt():
+    # create an ArgumentParser object
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", type=str, default="", help="GG Drive url")
+    parser.add_argument("--save_path", type=str, default="checkpoints/model.keras", help="save model")
+    opt = parser.parse_args()
+    
+    return opt
+
+def download_model(url, save_path):
+    # download model from Google Drive
+    gdown.download(url=url, output=save_path, quiet=False, fuzzy=True)
+    print("Model downloaded successfully.")
 
 if __name__ == "__main__":
-    # download model from Google Drive
-    gdown.download(url=model_url, output="checkpoints/100_sport_clf.keras", quiet=False, fuzzy=True)
-    print("Model downloaded successfully.")
+    opt = parse_opt()
+    download_model(**vars(opt))
