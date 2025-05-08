dbe705eba6f3a0312344e88012cb0c0cc7a8cf99
Toshi <huynhduykhoi619@gmail.com>
2025-05-08 15:14:22 +0700

    update script for prediction using CLI command

--- infer.py
+++ infer.py
@@ -1,18 +1,35 @@
 import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import keras
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, classification_report
 import config
 import warnings
-
 warnings.filterwarnings("ignore")
 
-if __name__ == "__main__":
-    clf_model = keras.models.load_model(config.model_path)
-    img = keras.utils.load_img(path="data/test_images/basketball_test_1.jpg", target_size=(config.img_width, config.img_heigth))
+import argparse
+from pathlib import Path
+import sys
+from data import merge_data
+
+def parse_opt():
+    # create an ArgumentParser object
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--weight", type=str, default="checkpoints/100_sport_clf.keras", help="model path(s)")
+    parser.add_argument("--source", type=str, default="data/test_images", help="file/dir/URL/glob/screen/0(webcam)")
+    opt = parser.parse_args()
+    
+    return opt
+
+def predict(weight, source):
+    clf_model = keras.models.load_model(weight)
+    img = keras.utils.load_img(path=source, target_size=(config.img_width, config.img_heigth))
     img = keras.utils.img_to_array(img)
     input_arr = np.array([img])
     predicts = clf_model.predict(input_arr, verbose=0) # [1, 100]
     predicted_class = np.argmax(predicts, axis=1)[0]
-    print(config.int2label[predicted_class])
+    print(f"Label of an image: {config.int2label[predicted_class]}")
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    predict(**vars(opt))
