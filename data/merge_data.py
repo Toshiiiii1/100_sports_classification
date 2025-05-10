2929a8e9c02dc77cb495477ad048523756e68a3e
Toshi <huynhduykhoi619@gmail.com>
2025-05-10 14:44:58 +0700

    script for automatically merge old data and new data for transfer learning

--- data/merge_data.py
+++ data/merge_data.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import random
+import argparse
 
 def copy_old_dataset(src_folder_path, des_folder_path, subset="train", ratio=1):
     os.makedirs(des_folder_path, exist_ok=True)
@@ -54,18 +55,31 @@ def copy_new_dataset(src_path, des_path, num_files=5):
                 img_src_path = os.path.join(folder_path, img_name)
                 des_img_path = os.path.join(des_folder_path, img_name)
                 shutil.copyfile(img_src_path, des_img_path)
-
-if __name__ == "__main__":
-    sport_22_path = "data/raw/22_sports_from_Konapure"
-    sport_100_path = "data/raw/100_sports"
-    des_path = "data/preprocessed/dataset_merged_1"
-    
+                
+def main(opt):
     # copy train set from 100 sports to preprocessed folder
-    copy_old_dataset(src_folder_path=sport_100_path, des_folder_path=des_path, ratio=1)
+    copy_old_dataset(src_folder_path=opt.old_data, des_folder_path=opt.save, ratio=opt.ratio)
     
     # copy entire valid set and test set from 100 sports to preprocessed folder
-    copy_old_dataset(src_folder_path=sport_100_path, des_folder_path=des_path, subset="valid", ratio=1)
-    copy_old_dataset(src_folder_path=sport_100_path, des_folder_path=des_path, subset="test", ratio=1)
+    copy_old_dataset(src_folder_path=opt.old_data, des_folder_path=opt.save, subset="valid", ratio=1)
+    copy_old_dataset(src_folder_path=opt.old_data, des_folder_path=opt.save, subset="test", ratio=1)
     
     # split new dataset to train set, valid set and test set. Copy all subset to megred folder
-    copy_new_dataset(src_path=sport_22_path, des_path=des_path)
+    copy_new_dataset(src_path=opt.new_data, des_path=opt.save)
+
+def parse_opt():
+    # create an ArgumentParser object
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--old-data", type=str, default="", help="old data path")
+    parser.add_argument("--ratio", type=float, default=0.5, help="proportion of the old dataset that was taken to merge")
+    parser.add_argument("--new-data", type=str, default="", help="new path")
+    parser.add_argument("--save", type=str, default="", help="save path")
+    
+    opt = parser.parse_args()
+    
+    return opt
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
