3cb55a767cc34834adf07bbbc85849a5fb362c80
Toshi <huynhduykhoi619@gmail.com>
2025-05-10 14:44:26 +0700

    automatically download model from ggdrive url instead of CLi command

--- checkpoints/checkpoint_download.py
+++ checkpoints/checkpoint_download.py
@@ -1,20 +1,7 @@
 import gdown
-import argparse
-
-def parse_opt():
-    # create an ArgumentParser object
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--url", type=str, default="", help="GG Drive url")
-    parser.add_argument("--save_path", type=str, default="checkpoints/model.keras", help="save model")
-    opt = parser.parse_args()
-    
-    return opt
-
-def download_model(url, save_path):
-    # download model from Google Drive
-    gdown.download(url=url, output=save_path, quiet=False, fuzzy=True)
-    print("Model downloaded successfully.")
+import config
+import sys
 
 if __name__ == "__main__":
-    opt = parse_opt()
-    download_model(**vars(opt))
+    gdown.download(url=config.model_ggdrive_path, output="checkpoints/clf_checkpoint.keras", quiet=False, fuzzy=True)
+    print("Model downloaded successfully.")
