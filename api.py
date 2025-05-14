9e257611062b59e98b4c78ec835871b2e6a2fd2a
Toshi <huynhduykhoi619@gmail.com>
2025-05-14 14:37:05 +0700

    create api for prediction

---
+++ api.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, UploadFile, HTTPException, File
+from pydantic import BaseModel
+import numpy as np
+from PIL import Image
+import io
+import config
+import keras
+import time
+
+app = FastAPI()
+clf_model = keras.saving.load_model("checkpoints/105_sport_clf.keras")
+
+# define API repsone schema
+class PredictRepsone(BaseModel):
+    label: str
+    confidence: float
+    time: float
+
+@app.get("/")
+async def root():
+    return {"message": "API works"}
+
+@app.post("/predict")
+async def predict(file: UploadFile):
+    # validate file type
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(status_code=400, detail="only except image file (png/jpg)")
+    
+    # read image as bytes type
+    img_bytes = io.BytesIO(await file.read())
+    # convert image bytes type to np array
+    image = Image.open(img_bytes).resize((config.img_width, config.img_heigth))
+    img_array = keras.utils.img_to_array(image)
+    batch_img = np.array([img_array]) # [1, 224, 224]
+    
+    # handle prediction fail
+    try:
+        start_time = time.time()
+        predictions = clf_model.predict(batch_img) # [1, 100]
+        end_time = time.time()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Prediction failed: {str(e)}")
+    
+    label_index = np.argmax(predictions, axis=1)[0]
+    label_prob = predictions[0][label_index] # np float32
+    label = config.int2label[label_index]
+    infer_time = (end_time - start_time) # second
+    
+    return PredictRepsone(
+        label=label,
+        confidence=float(label_prob),
+        time=infer_time,
+    )
